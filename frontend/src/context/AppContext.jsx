17beabea1b9be5b35b5f7ff6ebe900e4a825eef6
khalidnbg <khalidnabgaoui4@gmail.com>
2024-11-08 13:02:41 +0100

    add the profile data function to app context

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -11,6 +11,7 @@ const AppContextProvider = (props) => {
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(localStorage.getItem("token") || false);
+  const [userData, setUserData] = useState(false);
 
   const getDoctorsData = async () => {
     try {
@@ -26,11 +27,45 @@ const AppContextProvider = (props) => {
     }
   };
 
-  const value = { doctors, currencySymbol, token, setToken, backendUrl };
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
+  };
 
   useEffect(() => {
     getDoctorsData();
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
